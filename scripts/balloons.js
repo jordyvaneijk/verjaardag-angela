83c39bb895573f11ccd226b0d34941c691c081bf
Jordy van Eijk <jordy.vaneijk@teamrockstars.nl>
2023-01-13 00:55:32 +0100

    test

--- scripts/balloons.js
+++ scripts/balloons.js
@@ -40,42 +40,54 @@ function createBalloons(num) {
 function removeBalloons() {
   balloonContainer.style.opacity = 0;
   setTimeout(() => {
-    balloonContainer.remove()
-  }, 500)
+    balloonContainer.remove();
+  }, 500);
 }
 
-const steps = new Array(mainContainer,step1Container,step2Container,step3Container,step4Container,step5Container,step6Container,step7Container);
-
+const steps = new Array(
+  mainContainer,
+  step1Container,
+  step2Container,
+  step3Container,
+  step4Container,
+  step5Container,
+  step6Container,
+  step7Container
+);
 
 let currentStep = 0;
-function next(){
+
+function next() {
+  steps[currentStep].style.display = "none";
+  currentStep++;
+
+  if (currentStep >= steps.length) {
     steps[currentStep].style.display = "none";
-    steps[currentStep+1].style.display = "block";
-    currentStep++;
-    if(currentStep > steps.length){
-        currentStep=0;
-        steps[0].style.display = "block";
-    }
+    currentStep = 0;
+    steps[0].style.display = "block";
+  } else {
+    steps[currentStep].style.display = "block";
+  }
 }
 
-function step1(){
-    mainContainer.style.display = "none";
-    step1Container.style.display = "block";
+function step1() {
+  mainContainer.style.display = "none";
+  step1Container.style.display = "block";
 }
 
-function step2(){
-    step1Container.style.display = "none";
-    step2Container.style.display = "block";
+function step2() {
+  step1Container.style.display = "none";
+  step2Container.style.display = "block";
 }
 
-function step3(){
-    step2Container.style.display = "none";
-    step3Container.style.display = "block";
+function step3() {
+  step2Container.style.display = "none";
+  step3Container.style.display = "block";
 }
 
-function step4(){
-    step3Container.style.display = "none";
-    step4Container.style.display = "block";
+function step4() {
+  step3Container.style.display = "none";
+  step4Container.style.display = "block";
 }
 
 // window.addEventListener("load", () => {
